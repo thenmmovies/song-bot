56350b0d7cb25334ebd9d58f475ede076747f720
thenmmovies <97659554+thenmmovies@users.noreply.github.com>
2022-01-31 23:08:49 +0530

    Update song.py

--- modules/song.py
+++ modules/song.py
@@ -35,16 +35,7 @@ def time_to_seconds(time):
 @Client.on_message(filters.command(['start'])) 
 async def start(client, message):
     await message.reply_photo(photo=Config.START_IMG, caption=Config.START_MSG.format(message.from_user.mention),
-         reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(BUTTON1, url=GIT)
-                 ],[
-                    InlineKeyboardButton(OWNER, url=f"https://telegram.dog/{Config.OWNER}"),
-                    InlineKeyboardButton(DVP, url=B2)
-            ]
-          ]
-        ),
+         reply_markup=InlineKeyboardMarkup(button),
         reply_to_message_id=message.message_id
     )
 
