7ff9178302e8802dd680959de553b474c66310ae
thenmmovies <97659554+thenmmovies@users.noreply.github.com>
2022-02-01 11:58:12 +0530

    Update song.py

--- modules/song.py
+++ modules/song.py
@@ -38,7 +38,7 @@ async def start(client, message):
         reply_to_message_id=message.message_id
     )
 
- @Bot.on_callback_query()
+ @Client.on_callback_query()
 async def cb_handler(bot, update): 
   if update.data == "close":
   await update.message.delete()
